e09a3a58e09fef95394f6d367e17067e5d77d16b
YskaArt <franciscomorello99@gmail.com>
2025-04-29 02:54:14 -0300

    Mixamo Update
    Se metieron la siguientes mecanicas y animaciones correspondientes a cada una (aun falta pulirlas):
    -Sprint
    -Slide
    -Crouching/Crouching Move
    -Wall Jump
    Falta Implementar:
    -Dash

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,46 +1,250 @@
-using UnityEngine;
+﻿using UnityEngine;
 
-
-public class PlayerMovement : MonoBehaviour
+[RequireComponent(typeof(CharacterController))]
+public class PlayerController : MonoBehaviour
 {
-    public float speed = 6f;
-    public float jumpHeight = 2f;
-    public float gravity = -9.81f;
+    [Header("Movement Settings")]
+    public float walkSpeed = 4f;
+    public float sprintSpeed = 8f;
+    public float crouchSpeed = 2f;
+    public float slideSpeed = 10f;
+    public float jumpForce = 6f;
+    public float gravity = -20f;
 
-    private CharacterController controller;
-    private Vector3 velocity;
-    private bool isGrounded;
+    [Header("Crouch Settings")]
+    public float standingHeight = 2f;
+    public float crouchHeight = 1f;
 
+    [Header("Slide Settings")]
+    public float slideDuration = 0.8f;
+    private float slideTimer;
+
+    [Header("Wall Jump Settings")]
+    public LayerMask wallMask;
+    public float wallJumpForce = 8f;
+    public float wallJumpDirectionForce = 4f;
+    public float wallJumpDuration = 1.2f; // Tiempo que dura la fuerza lateral
+
+    [Header("Ground Check Settings")]
     public Transform groundCheck;
-    public float groundDistance = 0.4f;
+    public float groundCheckRadius = 0.3f;
     public LayerMask groundMask;
 
-    void Start()
+    [Header("Camera Settings")]
+    public Transform orientation;
+
+    private CharacterController controller;
+    private Animator animator;
+
+    private Vector3 moveDirection;
+    private Vector3 velocity;
+    private Vector3 slideDirection;
+    private Vector3 wallJumpDirection;
+    private float wallJumpTimer;
+
+    private bool isGrounded;
+    private bool isSprinting;
+    private bool isCrouching;
+    private bool isSliding;
+
+    private void Start()
     {
         controller = GetComponent<CharacterController>();
+        animator = GetComponentInChildren<Animator>();
     }
 
-    void Update()
+    private void Update()
     {
-       
-        isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
+        GroundCheck();
+        HandleInput();
+        HandleMovement();
+        HandleGravity();
+        HandleAnimations();
 
+        UpdateWallJumpTimer();
+    }
+
+    private void GroundCheck()
+    {
+        isGrounded = Physics.CheckSphere(groundCheck.position, groundCheckRadius, groundMask);
+        animator.SetBool("IsGrounded", isGrounded);
+    }
+
+    private void HandleInput()
+    {
+        float inputX = Input.GetAxis("Horizontal");
+        float inputZ = Input.GetAxis("Vertical");
+
+        Vector3 input = new Vector3(inputX, 0, inputZ).normalized;
+        moveDirection = orientation.forward * input.z + orientation.right * input.x;
+
+        // Sprint
+        isSprinting = Input.GetKey(KeyCode.LeftShift) && inputZ > 0 && isGrounded && !isCrouching && !isSliding;
+
+        // Jump
+        if (Input.GetButtonDown("Jump"))
+        {
+            if (isGrounded)
+            {
+                Jump();
+            }
+            else
+            {
+                TryWallJump();
+            }
+        }
+
+        // Crouch / Slide
+        if (Input.GetKeyDown(KeyCode.LeftControl))
+        {
+            if (isSprinting && isGrounded)
+            {
+                StartSlide();
+            }
+            else
+            {
+                StartCrouch();
+            }
+        }
+        else if (Input.GetKeyUp(KeyCode.LeftControl))
+        {
+            if (isSliding)
+                StopSlide();
+            else
+                StopCrouch();
+        }
+    }
+
+    private void HandleMovement()
+    {
+        Vector3 finalMove = moveDirection;
+
+        if (wallJumpTimer > 0)
+            finalMove += wallJumpDirection;
+
+        if (isSliding)
+        {
+            finalMove = slideDirection;
+            controller.Move(finalMove.normalized * slideSpeed * Time.deltaTime);
+            slideTimer -= Time.deltaTime;
+            if (slideTimer <= 0)
+            {
+                StopSlide();
+            }
+            return;
+        }
+
+        float currentSpeed = walkSpeed;
+        if (isCrouching)
+            currentSpeed = crouchSpeed;
+        else if (isSprinting)
+            currentSpeed = sprintSpeed;
+
+        controller.Move(finalMove.normalized * currentSpeed * Time.deltaTime);
+    }
+
+    private void HandleGravity()
+    {
         if (isGrounded && velocity.y < 0)
+        {
             velocity.y = -2f;
+        }
 
-        
-        float x = Input.GetAxis("Horizontal");
-        float z = Input.GetAxis("Vertical");
-
-        Vector3 move = transform.right * x + transform.forward * z;
-        controller.Move(move * speed * Time.deltaTime);
-
-        
-        if (Input.GetButtonDown("Jump") && isGrounded)
-            velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
-
-       
         velocity.y += gravity * Time.deltaTime;
         controller.Move(velocity * Time.deltaTime);
+    }
+
+    private void Jump()
+    {
+        velocity.y = Mathf.Sqrt(jumpForce * -2f * gravity);
+        animator.SetTrigger("Jump");
+    }
+
+    private void TryWallJump()
+    {
+        RaycastHit hit;
+        if (Physics.Raycast(transform.position, orientation.right, out hit, 2f, wallMask))
+        {
+            WallJump(-orientation.right, true);
+        }
+        else if (Physics.Raycast(transform.position, -orientation.right, out hit, 2f, wallMask))
+        {
+            WallJump(orientation.right, false);
+        }
+    }
+
+    private void WallJump(Vector3 wallNormal, bool mirror)
+    {
+        velocity.y = Mathf.Sqrt(wallJumpForce * -2f * gravity);
+        wallJumpDirection = wallNormal * wallJumpDirectionForce;
+        wallJumpTimer = wallJumpDuration;
+        animator.SetBool("WallJumpMirror", mirror);
+        animator.SetTrigger("WallJump");
+        
+    }
+
+    private void UpdateWallJumpTimer()
+    {
+        if (wallJumpTimer > 0)
+        {
+            wallJumpTimer -= Time.deltaTime;
+            if (wallJumpTimer <= 0)
+            {
+                wallJumpDirection = Vector3.zero;
+                animator.SetBool("WallJumpMirror", false);
+            }
+        }
+    }
+
+    private void StartCrouch()
+    {
+        isCrouching = true;
+        controller.height = crouchHeight;
+        controller.center = new Vector3(0, crouchHeight / 2f, 0);
+        animator.SetBool("IsCrouching", true);
+    }
+
+    private void StopCrouch()
+    {
+        isCrouching = false;
+        controller.height = standingHeight;
+        controller.center = new Vector3(0, standingHeight / 2f, 0);
+        animator.SetBool("IsCrouching", false);
+    }
+
+    private void StartSlide()
+    {
+        isSliding = true;
+        slideTimer = slideDuration;
+        controller.height = crouchHeight;
+        controller.center = new Vector3(0, crouchHeight / 2f, 0);
+        animator.SetTrigger("Slide");
+        slideDirection = moveDirection.magnitude > 0.1f ? moveDirection : orientation.forward;
+    }
+
+    private void StopSlide()
+    {
+        isSliding = false;
+        controller.height = standingHeight;
+        controller.center = new Vector3(0, standingHeight / 2f, 0);
+        animator.ResetTrigger("Slide");
+    }
+
+    private void HandleAnimations()
+    {
+        float inputX = Input.GetAxis("Horizontal");
+        float inputZ = Input.GetAxis("Vertical");
+
+        animator.SetFloat("MoveX", inputX, 0.1f, Time.deltaTime);
+        animator.SetFloat("MoveZ", inputZ, 0.1f, Time.deltaTime);
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        if (groundCheck != null)
+        {
+            Gizmos.color = Color.green;
+            Gizmos.DrawWireSphere(groundCheck.position, groundCheckRadius);
+        }
     }
 }
--- Assets/Scripts/ThirdPersonCamera.cs
+++ Assets/Scripts/ThirdPersonCamera.cs
@@ -2,9 +2,9 @@ using UnityEngine;
 
 public class ThirdPersonCamera : MonoBehaviour
 {
-    public Transform target;              
-    public Vector3 offset = new Vector3(); 
-    public float mouseSensitivity = 3f;    
+    public Transform target;
+    public Vector3 offset = new Vector3();
+    public float mouseSensitivity = 3f;
     public float rotationSmoothTime = 0.1f;
 
     private float currentYaw;
@@ -17,7 +17,7 @@ public class ThirdPersonCamera : MonoBehaviour
 
     void LateUpdate()
     {
-        
+
         float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity;
         float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity;
 
@@ -25,15 +25,15 @@ public class ThirdPersonCamera : MonoBehaviour
         currentPitch -= mouseY;
         currentPitch = Mathf.Clamp(currentPitch, minPitch, maxPitch);
 
-        
+
         Vector3 targetRotation = new Vector3(currentPitch, currentYaw);
         currentRotation = Vector3.SmoothDamp(currentRotation, targetRotation, ref rotationSmoothVelocity, rotationSmoothTime);
 
-        
+
         transform.position = target.position + Quaternion.Euler(currentRotation.x, currentRotation.y, 0) * offset;
         transform.LookAt(target.position + Vector3.up * 1.5f);
 
-        
+
         Vector3 lookDir = transform.position - target.position;
         lookDir.y = 0;
         target.rotation = Quaternion.LookRotation(-lookDir);
